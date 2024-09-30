ffe6f53fe4eff569c74f00a806de4b634bdfd763
Stuart Mumford <stuart@cadair.com>
2024-09-30 11:50:03 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,11 +2,13 @@
 Configuration file for the Sphinx documentation builder.
 """
 # -- stdlib imports ------------------------------------------------------------
+
 import os
 import datetime
 from packaging.version import Version
 
 # -- Read the Docs Specific Configuration --------------------------------------
+
 on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 if on_rtd:
     os.environ["SUNPY_CONFIGDIR"] = "/home/docs/"
@@ -15,14 +17,9 @@ if on_rtd:
     os.environ["LC_ALL"] = "C"
     os.environ["PARFIVE_HIDE_PROGRESS"] = "True"
 
-from packaging.version import Version
-
 # -- Project information -----------------------------------------------------
 
 # The full version, including alpha/beta/rc tags
-<<<<<<<
-from sunkit_instruments import __version__  # NOQA
-=======
 from sunkit_instruments import __version__
 
 _version = Version(__version__)
@@ -34,17 +31,13 @@ if _version.is_postrelease:
 elif _version.is_devrelease:
     version = release = f'{_version.base_version}.dev{_version.dev}'
 is_development = _version.is_devrelease
->>>>>>>
 
 project = "sunkit-instruments"
 author = "The SunPy Community"
 copyright = f"{datetime.datetime.now().year}, {author}"
-release = __version__
-sunkit_instruments_version = Version(__version__)
-is_release = not (
-    sunkit_instruments_version.is_prerelease or sunkit_instruments_version.is_devrelease
-)
+
 # -- General configuration -----------------------------------------------------
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
@@ -85,9 +78,10 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # Treat everything in single ` as a Python reference.
-default_role = 'py:obj'
+default_role = "py:obj"
 
 # -- Options for intersphinx extension -----------------------------------------
+
 intersphinx_mapping = {
     "python": (
         "https://docs.python.org/3/",
@@ -118,7 +112,6 @@ intersphinx_mapping = {
     "reproject": ("https://reproject.readthedocs.io/en/stable/", None),
     "aiapy": ("https://aiapy.readthedocs.io/en/stable/", None),
 }
-default_role = "obj"
 
 # -- Options for HTML output ---------------------------------------------------
 
@@ -126,9 +119,22 @@ from sunpy_sphinx_theme import PNG_ICON
 
 html_theme = "sunpy"
 
-# -- Other options ----------------------------------------------------------
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
+
+# By default, when rendering docstrings for classes, sphinx.ext.autodoc will
+# make docs with the class-level docstring and the class-method docstrings,
+# but not the __init__ docstring, which often contains the parameters to
+# class constructors across the scientific Python ecosystem. The option below
+# will append the __init__ docstring to the class-level docstring when rendering
+# the docs. For more options, see:
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
+autoclass_content = "both"
 
 # -- Graphviz Output ------------------------------------------------------------
+
 graphviz_output_format = "svg"
 graphviz_dot_args = [
     "-Nfontsize=10",
@@ -140,6 +146,7 @@ graphviz_dot_args = [
 ]
 
 # -- Sphinx Gallery ------------------------------------------------------------
+
 sphinx_gallery_conf = {
     "backreferences_dir": os.path.join("generated", "modules"),
     "filename_pattern": "^((?!skip_).)*$",
@@ -153,3 +160,5 @@ sphinx_gallery_conf = {
     "doc_module": ("sunpy"),
     "only_warn_on_example_error": True,
 }
+
+# -- Other options ----------------------------------------------------------
